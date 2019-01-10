c2ba901b8210866099a4231c61100d54be124fa0
Bo Wang <logo-wang@163.com>
2019-01-10 14:53:32 +0800

    Update 04 Counting.sh

--- 04 Counting.sh
+++ 04 Counting.sh
@@ -2,3 +2,13 @@
 #######  Fourth Step  ###########
 #### Counting the Align reads ###
 #################################
+
+### convert *.sam to *.bam ###
+ls *.sam | while read id ;do (samtools sort -O bam @ -10 $(basename ${id} ".sam").bam ${id}; done
+ls *.bam | xargs -i samtools index {}
+
+####### qc analysis #######
+ls *.bam | while read id ;do (samtools flagstat @ -20 $id > $(basename ${id} ".sam").flagstat); done
+# samtools view *.bam | less -SN
+# multiqc *.flagstat
+
